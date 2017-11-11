e00ef6e2d30f872bce26e3ed5d1dbe88ce3b4c1a
David Driscoll <david.driscoll@gmail.com>
2017-11-11 09:55:55 -0500

    initial commit

---
+++ src/AspNet.Hosting/HostingEnvironment.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Rocket.Surgery.Extensions.Hosting
+{
+    public static class HostingEnvironmentExtensions
+    {
+        public static IHostingEnvironment ToRocketSurgeryHostingEnvironment(this Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            return new HostingEnvironment(
+                environment.EnvironmentName,
+                environment.ApplicationName,
+                environment.WebRootPath,
+                environment.ContentRootPath);
+        }
+    }
+}
---
+++ src/Hosting/HostingEnvironment.cs
@@ -0,0 +1,18 @@
+﻿namespace Rocket.Surgery.Extensions.Hosting
+{
+    public class HostingEnvironment : IHostingEnvironment
+    {
+        public HostingEnvironment(string environmentName, string applicationName, string webRootPath, string contentRootPath)
+        {
+            EnvironmentName = environmentName;
+            ApplicationName = applicationName;
+            WebRootPath = webRootPath;
+            ContentRootPath = contentRootPath;
+        }
+
+        public string EnvironmentName { get;  }
+        public string ApplicationName { get; }
+        public string WebRootPath { get;  }
+        public string ContentRootPath { get; }
+    }
+}
