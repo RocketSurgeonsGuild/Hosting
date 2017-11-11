facbe877402d311c23be3e65ed9a8fcc69c2f1b5
David Driscoll <david.driscoll@gmail.com>
2017-11-11 10:12:05 -0500

    Added tests and readme

---
+++ test/Hosting.Tests/SimpleTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace Rocket.Surgery.Extensions.Hosting.Tests
+{
+    public class SimpleTest
+    {
+        [Fact]
+        public void ConvertsFromAspNetCoreHosting_To_RocketSurgeryHosting()
+        {
+            var fake = A.Fake<Microsoft.AspNetCore.Hosting.IHostingEnvironment>();
+            A.CallTo(() => fake.EnvironmentName).Returns(nameof(IHostingEnvironment.EnvironmentName));
+            A.CallTo(() => fake.ApplicationName).Returns(nameof(IHostingEnvironment.ApplicationName));
+            A.CallTo(() => fake.ContentRootPath).Returns(nameof(IHostingEnvironment.ContentRootPath));
+            A.CallTo(() => fake.WebRootPath).Returns(nameof(IHostingEnvironment.WebRootPath));
+
+            var environment = fake.ToRocketSurgeryHostingEnvironment();
+            environment.EnvironmentName.Should().Be(nameof(IHostingEnvironment.EnvironmentName));
+            environment.ApplicationName.Should().Be(nameof(IHostingEnvironment.ApplicationName));
+            environment.ContentRootPath.Should().Be(nameof(IHostingEnvironment.ContentRootPath));
+            environment.WebRootPath.Should().Be(nameof(IHostingEnvironment.WebRootPath));
+        }
+    }
+}
