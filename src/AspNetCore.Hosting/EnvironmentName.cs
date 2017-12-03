b7540d9b3fc5ac9b9cd2df735d9e098daa7b815c
David Driscoll <david.driscoll@gmail.com>
2017-12-03 00:06:54 -0500

    Added extension methods for environment names

---
+++ src/AspNetCore.Hosting/EnvironmentName.cs
@@ -0,0 +1,10 @@
+﻿namespace Rocket.Surgery.AspNetCore.Hosting
+{
+    public static class EnvironmentName
+    {
+        public static readonly string Development = "Development";
+        public static readonly string Test = "Test";
+        public static readonly string Staging = "Staging";
+        public static readonly string Production = "Production";
+    }
+}
--- src/AspNetCore.Hosting/HostingEnvironmentExtensions.cs
+++ src/AspNetCore.Hosting/HostingEnvironmentExtensions.cs
@@ -1,5 +1,9 @@
-﻿using IRsgHostingEnvironment = Rocket.Surgery.Hosting.IHostingEnvironment;
+﻿using System;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Rocket.Surgery.AspNetCore.Hosting;
+using IRsgHostingEnvironment = Rocket.Surgery.Hosting.IHostingEnvironment;
 using RsgHostingEnvironment = Rocket.Surgery.Hosting.HostingEnvironment;
+using RsgEnvironmentName = Rocket.Surgery.AspNetCore.Hosting.EnvironmentName;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Hosting
@@ -13,6 +17,87 @@ namespace Microsoft.AspNetCore.Hosting
                 environment.ApplicationName,
                 environment.WebRootPath,
                 environment.ContentRootPath);
+        }
+
+        /// <summary>
+        /// Checks if the current hosting environment name is <see cref="RsgEnvironmentName.Development"/>.
+        /// </summary>
+        /// <param name="hostingEnvironment">An instance of <see cref="IHostingEnvironment"/>.</param>
+        /// <returns>True if the environment name is <see cref="RsgEnvironmentName.Development"/>, otherwise false.</returns>
+        public static bool IsDevelopment(this IRsgHostingEnvironment hostingEnvironment)
+        {
+            if (hostingEnvironment == null)
+            {
+                throw new ArgumentNullException(nameof(hostingEnvironment));
+            }
+
+            return hostingEnvironment.IsEnvironment(RsgEnvironmentName.Development);
+        }
+
+        /// <summary>
+        /// Checks if the current hosting environment name is <see cref="RsgEnvironmentName.Test"/>.
+        /// </summary>
+        /// <param name="hostingEnvironment">An instance of <see cref="IHostingEnvironment"/>.</param>
+        /// <returns>True if the environment name is <see cref="RsgEnvironmentName.Test"/>, otherwise false.</returns>
+        public static bool IsTest(this IRsgHostingEnvironment hostingEnvironment)
+        {
+            if (hostingEnvironment == null)
+            {
+                throw new ArgumentNullException(nameof(hostingEnvironment));
+            }
+
+            return hostingEnvironment.IsEnvironment(RsgEnvironmentName.Test);
+        }
+
+        /// <summary>
+        /// Checks if the current hosting environment name is <see cref="RsgEnvironmentName.Staging"/>.
+        /// </summary>
+        /// <param name="hostingEnvironment">An instance of <see cref="IHostingEnvironment"/>.</param>
+        /// <returns>True if the environment name is <see cref="RsgEnvironmentName.Staging"/>, otherwise false.</returns>
+        public static bool IsStaging(this IRsgHostingEnvironment hostingEnvironment)
+        {
+            if (hostingEnvironment == null)
+            {
+                throw new ArgumentNullException(nameof(hostingEnvironment));
+            }
+
+            return hostingEnvironment.IsEnvironment(RsgEnvironmentName.Staging);
+        }
+
+        /// <summary>
+        /// Checks if the current hosting environment name is <see cref="RsgEnvironmentName.Production"/>.
+        /// </summary>
+        /// <param name="hostingEnvironment">An instance of <see cref="IHostingEnvironment"/>.</param>
+        /// <returns>True if the environment name is <see cref="RsgEnvironmentName.Production"/>, otherwise false.</returns>
+        public static bool IsProduction(this IRsgHostingEnvironment hostingEnvironment)
+        {
+            if (hostingEnvironment == null)
+            {
+                throw new ArgumentNullException(nameof(hostingEnvironment));
+            }
+
+            return hostingEnvironment.IsEnvironment(RsgEnvironmentName.Production);
+        }
+
+        /// <summary>
+        /// Compares the current hosting environment name against the specified value.
+        /// </summary>
+        /// <param name="hostingEnvironment">An instance of <see cref="IHostingEnvironment"/>.</param>
+        /// <param name="environmentName">Environment name to validate against.</param>
+        /// <returns>True if the specified name is the same as the current environment, otherwise false.</returns>
+        public static bool IsEnvironment(
+            this IRsgHostingEnvironment hostingEnvironment,
+            string environmentName)
+        {
+            if (hostingEnvironment == null)
+            {
+                throw new ArgumentNullException(nameof(hostingEnvironment));
+            }
+
+            return string.Equals(
+                hostingEnvironment.EnvironmentName,
+                environmentName,
+                StringComparison.OrdinalIgnoreCase);
         }
     }
 }
